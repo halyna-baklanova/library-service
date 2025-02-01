8bb9c156c3324931617b85bc3ae9cdc17cf07f82
halyna-baklanova <novabalka@gmail.com>
2025-02-01 22:27:43 +0200

    add the borrowing model with constraints

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from book_service.models import Book
+from user.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, related_name="borrowing_book"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="borrowing_user"
+    )
+
+    def __str__(self):
+        return f"Borrowing: {self.book} by User: {self.user}"
