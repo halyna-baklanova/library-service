e96f4aef53e7c3ccc548213e8ba98b7532ad499e
halyna-baklanova <novabalka@gmail.com>
2025-02-01 18:53:47 +0200

    implement models for Book

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+import pathlib
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+
+def book_cover_path(instance: "Book", filename: str) -> pathlib.Path:
+    extension = pathlib.Path(filename).suffix
+    filename = f"{slugify(instance.title)}-{uuid.uuid4()}" + extension
+    return pathlib.Path("uploads/actors/") / pathlib.Path(filename)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    cover = models.ImageField(upload_to=book_cover_path, null=True, blank=True)
+    inventory = models.PositiveIntegerField(default=0)
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
