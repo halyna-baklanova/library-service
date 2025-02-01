c882d2deb865e9e1b7e10dee69a6689249deab2d
halyna-baklanova <novabalka@gmail.com>
2025-02-01 21:01:30 +0200

    implement user model with email and  JWT support

--- library_service/urls.py
+++ library_service/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/book_service/", include("book_service.urls", namespace="book_service")),
+    path("api/user/", include("user.urls", namespace="user")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
