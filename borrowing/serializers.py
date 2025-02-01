d79cff822dc57a94d7c43c1844720cfee82765b0
halyna-baklanova <novabalka@gmail.com>
2025-02-01 23:15:50 +0200

    Implement create serializer Borrowing

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,18 +1,23 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Borrowing
-from book_service.serializers import BookSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
-    book = BookSerializer(read_only=True)
-
     class Meta:
         model = Borrowing
-        fields = [
-            "id",
-            "book",
-            "user",
-            "borrow_date",
-            "expected_return_date",
-            "actual_return_date",
-        ]
+        fields = ["id", "user", "book", "book_title", "book_author"]
+        read_only_fields = ["user", "borrowed_at", "returned_at"]
+
+    def validate(self, data):
+        if data["book"].inventory == 0:
+            raise ValidationError("This book is out of stock.")
+        return data
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+        validated_data["user"] = self.context["request"].user
+        return super().create(validated_data)
