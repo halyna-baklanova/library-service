275237d55bf40e261a44cad021d67d83c95893ee
halyna-baklanova <novabalka@gmail.com>
2025-02-01 20:07:22 +0200

    create serializers for Book

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "book_service",
+    "rest_framework" "book_service",
 ]
 
 MIDDLEWARE = [
