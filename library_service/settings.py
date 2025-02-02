4f6b9a24ea52a8839d3a8909a9ad319b71d00851
halyna-baklanova <novabalka@gmail.com>
2025-02-02 08:28:30 +0200

    create .env with settings

--- library_service/settings.py
+++ library_service/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 AUTH_USER_MODEL = "user.User"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jah)&shp@hs7(ke4_iuq3cp4%fn!pllf%w4w+y+=ezjpf7twhn"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
